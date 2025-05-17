dbcf93997338bbf66bb3c64fd08105b11882ca86
xgorfo <xgorfo@gmail.com>
2025-05-17 16:12:04 +0300

    add new products

--- src/data/products.js
+++ src/data/products.js
@@ -3,6 +3,11 @@ export const products = [
   { id: 2, name: "Туфли Classic", price: 90, category: "Туфли", description: "Классические туфли", image: "https://i.pinimg.com/736x/8f/da/49/8fda495b05c9409240a47efda529d3e9.jpg" },
   { id: 3, name: "Ботинки Trekking", price: 150, category: "Ботинки", description: "Прочные и теплые", image: "https://i.pinimg.com/736x/36/2a/5b/362a5bc3f33708078dba6a607a920d4e.jpg" },
   { id: 4, name: "Сандалии Summer", price: 60, category: "Сандалии", description: "Для лета и отдыха", image: "https://i.pinimg.com/736x/37/b9/57/37b957db5742e04539fbeee2823de0c4.jpg" },
-  { id: 5, name: "Мокасины Soft", price: 80, category: "Мокасины", description: "Мягкие и комфортные", image: "https://i.pinimg.com/736x/8b/16/c3/8b16c3210db7335ee7a247981104bbf3.jpg" },
-  { id: 6, name: "Слипоны Urban", price: 70, category: "Слипоны", description: "Городской стиль", image: "https://i.pinimg.com/736x/af/34/1b/af341b0ae748c5ac9fc39ed488b5acce.jpg" },
+  { id: 5, name: "Мокасины Soft", price: 80, category: "Мокасины", description: "Мягкие и комфортные", image: "https://a.lmcdn.ru/img600x866/M/P/MP002XW0SU1W_27570784_1_v2_2x.jpg" },
+  { id: 6, name: "Слипоны Urban", price: 70, category: "Слипоны", description: "Городской стиль", image: "https://a.lmcdn.ru/img600x866/M/P/MP002XW0SQZI_27390268_2_v2_2x.jpg" },
+  { id: 7, name: "Кроссовки Jordan", price: 200, category: "Кроссовки", description: "Удобные и стильные", image: "https://a.lmcdn.ru/img600x866/R/T/RTLADY974701_25887329_1_v3_2x.jpg" },
+  { id: 8, name: "Туфли Mario Berlucci", price: 110, category: "Туфли", description: "Классические туфли", image: "https://marioberlucci.ru/upload/iblock/8ab/y2ghh5e5zgfdr9efnyxti645a5szc78e/1_G067_01-_2_.jpg?size=774x1128" },
+  { id: 9, name: "Туфли Mario Berlucci", price: 150, category: "Туфли", description: "Классические туфли", image: "https://marioberlucci.ru/upload/iblock/912/6g6w7gu1gx7rkus52nss3bq15xpnl4ru/1_G008_011-_1_.jpg?size=774x1128" },
+  { id: 10, name: "Кроссовки AIR JORDAN 11 RETRO", price: 300, category: "Кроссовки", description: "Удобные и стильные", image: "https://a.lmcdn.ru/img600x866/R/T/RTLADY974501_25887324_1_v1_2x.jpg" },
+
 ];
